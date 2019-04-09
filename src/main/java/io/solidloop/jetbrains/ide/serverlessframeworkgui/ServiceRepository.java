e5225d44472d2779eea10908193615d40352abe4
Plamen Paskov <paskov@gmail.com>
2019-04-09 18:58:37 +0300

    use FileTypeIndex to get all serverless.yaml files

--- src/main/java/io/solidloop/jetbrains/ide/serverlessframeworkgui/ServiceRepository.java
+++ src/main/java/io/solidloop/jetbrains/ide/serverlessframeworkgui/ServiceRepository.java
@@ -1,50 +1,54 @@
 package io.solidloop.jetbrains.ide.serverlessframeworkgui;
 
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.GlobalSearchScope;
 import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.yaml.YAMLFileType;
 
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 @AllArgsConstructor
 public class ServiceRepository {
-    private Project project;
     private ServiceFactory serviceFactory;
 
     public Set<Service> getAll() {
         Set<Service> services = new HashSet<>();
 
-        ProjectFileIndex.getInstance(project).iterateContent(file -> {
-            if (!file.isDirectory() && ServerlessFileUtil.isServerlessFile(file)) {
-                try {
-                    services.add(serviceFactory.create(file));
-                } catch (IOException e) {
-                    NotificationUtil.displayError("Parse error", file.getCanonicalPath() + " is invalid and cannot be parsed");
-                }
+        FileTypeIndex.getFiles(YAMLFileType.YML, new GlobalSearchScope() {
+            @Override
+            public int compare(@NotNull final VirtualFile file1, @NotNull final VirtualFile file2) {
+                return 0;
             }
 
-            return true;
+            @Override
+            public boolean isSearchInModuleContent(@NotNull final Module aModule) {
+                return false;
+            }
+
+            @Override
+            public boolean isSearchInLibraries() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(@NotNull final VirtualFile file) {
+                if (file.getExtension() != null) {
+                    return file.getName().substring(0, file.getName().length() - file.getExtension().length() - 1).equals("serverless");
+                }
+
+                return false;
+            }
+        }).forEach(file -> {
+            try {
+                services.add(serviceFactory.create(file));
+            } catch (ServiceException e) {
+            }
         });
 
         return services;
-    }
-
-    public Set<Service> filterBy(VirtualFile dir) {
-        Set<Service> filteredServices = new HashSet<>();
-        getAll().iterator().forEachRemaining(service -> {
-            if (!fileResidesInDir(service.getFile(), dir)) {
-                filteredServices.add(service);
-            }
-        });
-
-        return filteredServices;
-    }
-
-    private boolean fileResidesInDir(VirtualFile file, VirtualFile dir) {
-        String filePath = file.getCanonicalPath();
-        return !(filePath != null && filePath.startsWith(dir.getCanonicalPath() + "/"));
     }
 }
